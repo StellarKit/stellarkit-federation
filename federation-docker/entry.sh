d6c96636bf4a0691721c4c91327f77465d6e0ad9
Steve Gehrman <sgehrman@gmail.com>
2018-01-09 14:33:55 -0800

    sdf

--- federation-docker/entry.sh
+++ federation-docker/entry.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 set -e
 
 function main() {
-  while ! psql -h federationpostgres -U federation -c 'select 1' federationdb &> /dev/null ; do
-    echo "Waiting for bifrostdb to be available..."
+  PGPASSWORD=password
+  while ! psql -h federationpostgres -U federation -c 'select 1' federationdb &> /dev/null
+  do
+    echo "Waiting for federationdb to be available..."
     sleep 1
   done
 
@@ -13,13 +15,22 @@ function main() {
 }
 
 function init_federation_db() {
-	if [ -f /opt/federation/.federationdb-initialized ]; then
-		echo "federation DB: already initialized"
-		return 0
-	fi
+  if [ -f /opt/federation/.federationdb-initialized ]; then
+    echo "federation DB: already initialized"
+    return 0
+  fi
 
-	touch /opt/federation/.federationdb-initialized
- }
+  # add sample data
+  PGPASSWORD=password
+  psql -h federationpostgres -U federation federationdb -e <<-EOS
+    CREATE TABLE people (id character varying, name character varying, domain character varying);
+    INSERT INTO people (id, name, domain) VALUES
+      ('GD2GJPL3UOK5LX7TWXOACK2ZPWPFSLBNKL3GTGH6BLBNISK4BGWMFBBG', 'bob', 'stellar.org'),
+      ('GCYMGWPZ6NC2U7SO6SMXOP5ZLXOEC5SYPKITDMVEONLCHFSCCQR2J4S3', 'alice', 'stellar.org');
+  EOS
+
+  touch /opt/federation/.federationdb-initialized
+}
 
 function start_federation() {
   build-config /config.toml > /opt/federation/federation.cfg
